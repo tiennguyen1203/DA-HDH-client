414bb5c81f6f997f51d22eef445e04943c13e3b7
Tien Nguyen <mac@Macs-Macbook-Pro-2.local>
2020-11-21 19:41:26 +0700

    Add project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import 'antd/dist/antd.css';
+import React, { useEffect, useState } from 'react';
+import socketIOClient from 'socket.io-client';
 import './App.css';
+import './index.css';
+
+const ENDPOINT_SERVER = 'http://localhost:3001'
+const socket = socketIOClient(ENDPOINT_SERVER);
 
 function App() {
+  const [value, setValue] = useState("Input values you want calculate");
+
+  const onClick = () => {
+    const input = document.getElementById("input");
+    socket.emit('client-sent-data', (input.value));
+  }
+  useEffect(() => {
+    socket.on("server-sent-data", (data) => {
+      setValue(data);
+    });
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form">
+      <form >
+        <input
+          id="input"
+          type="text"
+        ></input>
+        <input
+          type="button"
+          onClick={onClick}
+          value="Calculate"
+        ></input>
+      </form>
+      <div>
+        {value}
+      </div>
     </div>
   );
 }
